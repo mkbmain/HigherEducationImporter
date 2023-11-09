eba57785e554d337e4d2507e46002458c759f600
mkbmain <michael.bourke@live.co.uk>
2023-11-09 17:50:03 +0000

    Remove preload .. this is a one time run per file it does not support dupe check so preloading is pointless

--- ImportStudentDataVB/Db/StudentBreakDownDb/StudentBreakDownImporter.vb
+++ ImportStudentDataVB/Db/StudentBreakDownDb/StudentBreakDownImporter.vb
@@ -9,6 +9,7 @@ Namespace Db.StudentBreakDownDb
         Private ReadOnly Property Repo As SqlRepo
 
         Public Sub New(repo as SqlRepo)
+            LookUp = New Dictionary(Of Type,Dictionary(Of String,BaseLookUpTable))()
             Me.Repo = repo
         End Sub
 
@@ -21,9 +22,9 @@ Namespace Db.StudentBreakDownDb
         Public Sub ImportCsv_BuildDb() Implements IImportDataToDb.BuildDb
             repo.Execute(File.ReadAllText("Db/StudentBreakDownDb/SqlToCreateDb/Create.sql"))
         End Sub
-        
+
         Public Sub ImportDataFromFile(location as String) Implements IImportDataToDb.ImportDataFromFile
-            LookUp = PreloadDbLookups(repo)
+            
             if File.Exists(location) = False Then
                 Console.Write("input data does not exist")
                 Return
@@ -40,24 +41,13 @@ Namespace Db.StudentBreakDownDb
             Const query =
                       "insert into Raw (LevelOfStudyId, FirstYearMarkerId, ModeOfStudyId, CountryId, SexId, DomicileId, AcademicYearId, Number, Percentage) values "
             dim sqlValues = raws.Select(Function(e) _
-                                           $"({e.LevelOfStudyId},{e.FirstYearMarkerId},{e.ModeOfStudyId},{e.CountryId}," +
-                                           $"{e.SexId},{e.DomicileId},{e.AcademicYearId},{e.Number},{e.Percentage})")
+                                           $"({e.LevelOfStudyId},{e.FirstYearMarkerId},{e.ModeOfStudyId _
+                                               },{e.CountryId}," +
+                                           $"{e.SexId},{e.DomicileId},{e.AcademicYearId},{e.Number},{ _
+                                               e.Percentage})")
 
             repo.Execute(query + String.Join(",", sqlValues))
         End Sub
-
-        private Shared Function PreloadDbLookups(repo as SqlRepo) _
-            as Dictionary(Of Type,Dictionary(Of String,BaseLookUpTable))
-            dim lookupDict = New Dictionary(Of Type,Dictionary(Of String,BaseLookUpTable))()
-            lookupDict.Add(GetType(AcademicYear), repo.GetAllOfTAsLookup (of AcademicYear)())
-            lookupDict.Add(GetType(Country), repo.GetAllOfTAsLookup (of Country)())
-            lookupDict.Add(GetType(Domicile), repo.GetAllOfTAsLookup (of Domicile)())
-            lookupDict.Add(GetType(FirstYearMarker), repo.GetAllOfTAsLookup (of FirstYearMarker)())
-            lookupDict.Add(GetType(LevelOfStudy), repo.GetAllOfTAsLookup (of LevelOfStudy)())
-            lookupDict.Add(GetType(Sex), repo.GetAllOfTAsLookup (of Sex)())
-            lookupDict.Add(GetType(ModeOfStudy), repo.GetAllOfTAsLookup (of ModeOfStudy)())
-            return lookupDict
-        End Function
 
         private Shared Function LineToRaw(line as String, repo As SqlRepo,
                                           lookup As Dictionary(Of Type,Dictionary(Of String,BaseLookUpTable))) As Raw
