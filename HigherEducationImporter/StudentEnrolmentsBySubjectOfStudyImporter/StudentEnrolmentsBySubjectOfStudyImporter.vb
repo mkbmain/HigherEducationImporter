5609b3c23d9d81f4bfc44f02fcc27de4b4f532b5
mkbmain <michael.bourke@live.co.uk>
2023-11-13 20:03:59 +0000

    few changes

--- HigherEducationImporter/StudentEnrolmentsBySubjectOfStudyImporter/StudentEnrolmentsBySubjectOfStudyImporter.vb
+++ HigherEducationImporter/StudentEnrolmentsBySubjectOfStudyImporter/StudentEnrolmentsBySubjectOfStudyImporter.vb
@@ -1,6 +1,5 @@
 Imports System.IO
 Imports ImportStudentDataVB.BaseDb
-Imports ImportStudentDataVB.StudentEnrolmentsByLevelOfStudyImporter.DbModels
 Imports ImportStudentDataVB.StudentEnrolmentsBySubjectOfStudyImporter.DbModels
 Imports Mkb.DapperRepo.Repo
 
@@ -9,38 +8,38 @@ Namespace StudentEnrolmentsBySubjectOfStudyImporter
         Implements IImporter
         Private ReadOnly Property Repo As SqlRepo
 
-        Public Sub New(repo as SqlRepo)
+        Public Sub New(repo As SqlRepo)
             Me.Repo = repo
         End Sub
 
-        Public readonly Property Name() As String Implements IImporter.Name
+        Public ReadOnly Property Name As String Implements IImporter.Name
             Get
                 Return NameOf(StudentEnrolmentsBySubjectOfStudyImporter)
             End Get
         End Property
 
         Public Sub ImportCsv_BuildDb() Implements IImporter.BuildDb
-            repo.Execute(File.ReadAllText("StudentEnrolmentsBySubjectOfStudyImporter/SqlToCreateDb/Create.sql"))
+            Repo.Execute(File.ReadAllText("StudentEnrolmentsBySubjectOfStudyImporter/SqlToCreateDb/Create.sql"))
         End Sub
 
-        Public Sub ImportDataFromFile(location as String) Implements IImporter.ImportDataFromFile
+        Public Sub ImportDataFromFile(location As String) Implements IImporter.ImportDataFromFile
 
-            if File.Exists(location) = False Then
+            If File.Exists(location) = False Then
                 Console.Write("input data does not exist")
                 Return
             End If
 
-            for Each item in _
-                GetRowsOfT (of StudentEnrolmentsBySubjectOfStudy)(location, function(line)  LineToStudentEnrolmentsBySubjectOfStudy(line, repo),15) _
-                .Where(Function(e) e isnot nothing).Chunk(250)
-                BulkInsertRaw(item, repo)
+            For Each item In
+                GetRowsOfT(location, Function(line) LineToStudentEnrolmentsBySubjectOfStudy(line, Repo), 15) _
+                .Where(Function(e) e IsNot Nothing).Chunk(250)
+                BulkInsertRaw(item, Repo)
             Next
         End Sub
 
-        private Shared Sub BulkInsertRaw(raws As IEnumerable(Of StudentEnrolmentsBySubjectOfStudy), repo As SqlRepo)
+        Private Shared Sub BulkInsertRaw(raws As IEnumerable(Of StudentEnrolmentsBySubjectOfStudy), repo As SqlRepo)
             Const query =
                       "insert into StudentEnrolmentsBySubjectOfStudy (CahLevelMarkerId, CAHLevelSubjectId, LevelOfStudyId, ModeOfStudyId, AcademicYearId, CategoryMarkerId, CategoryId, Number) values "
-            dim sqlValues = raws.Select(Function(e) _
+            Dim sqlValues = raws.Select(Function(e) _
                                            $"({e.CahLevelMarkerId},{e.CAHLevelSubjectId},{e.LevelOfStudyId _
                                                },{e.ModeOfStudyId}," +
                                            $"{e.AcademicYearId},{e.CategoryMarkerId},{e.CategoryId},{e.Number})")
@@ -48,19 +47,19 @@ Namespace StudentEnrolmentsBySubjectOfStudyImporter
             repo.Execute(query + String.Join(",", sqlValues))
         End Sub
 
-        private Shared Function LineToStudentEnrolmentsBySubjectOfStudy(line as String, repo As SqlRepo) As StudentEnrolmentsBySubjectOfStudy
-            dim parts as String() = line.Split(",")
-            if parts.Length <> 8 Then
+        Private Shared Function LineToStudentEnrolmentsBySubjectOfStudy(line As String, repo As SqlRepo) As StudentEnrolmentsBySubjectOfStudy
+            Dim parts As String() = line.Split(",")
+            If parts.Length <> 8 Then
                 Return Nothing
             End If
-            Return new StudentEnrolmentsBySubjectOfStudy With{
-                .CahLeveLMarkerId = repo.FindOrCreate ( of CahLevelMarker)(parts(0)),
-                .CAHLevelSubjectId =repo.FindOrCreate ( of CahLevelSubject)(parts(1)),
-                .LevelOfStudyId =repo.FindOrCreate ( of LevelOfStudy)(parts(2)),
-                .ModeOfStudyId =repo.FindOrCreate ( of ModeOfStudy)(parts(3)),
-                .AcademicYearId = repo.FindOrCreate ( of AcademicYear)(parts(4)),
-                .CategoryMarkerId = repo.FindOrCreate ( of CategoryMarker)(parts(5)),
-                .CategoryId = repo.FindOrCreate ( of Category)(parts(6)),
+            Return New StudentEnrolmentsBySubjectOfStudy With {
+                .CahLevelMarkerId = repo.FindOrCreate(Of CahLevelMarker)(parts(0)),
+                .CAHLevelSubjectId = repo.FindOrCreate(Of CahLevelSubject)(parts(1)),
+                .LevelOfStudyId = repo.FindOrCreate(Of LevelOfStudy)(parts(2)),
+                .ModeOfStudyId = repo.FindOrCreate(Of ModeOfStudy)(parts(3)),
+                .AcademicYearId = repo.FindOrCreate(Of AcademicYear)(parts(4)),
+                .CategoryMarkerId = repo.FindOrCreate(Of CategoryMarker)(parts(5)),
+                .CategoryId = repo.FindOrCreate(Of Category)(parts(6)),
                 .Number = Integer.Parse(parts(7))}
         End Function
     End Class
